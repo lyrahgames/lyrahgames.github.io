3b7053b803e3981153ec9f2d9011d0408ad7b658
lyrahgames <lyrahgames@mailbox.org>
2021-01-04 02:58:01 +0100

    Use responsive off-screen side bar

--- scripts/site.js
+++ scripts/site.js
@@ -1,8 +1,25 @@
-function site_nav_responsive_toggle() {
-  var x = document.getElementById('site-nav');
-  if (x.className === 'site-nav') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'site-nav';
-  }
+function open_site_nav() {
+  document.querySelector('html').style.overflow = 'hidden';
+  document.getElementById('site-nav-links').style.width = '250px';
+
+  document.getElementById('site-nav-close-area').style.width = '100%';
+  document.getElementById('site-nav-close-area').style.backgroundColor =
+    '#2225';
+}
+
+function close_site_nav_transitionend_callback() {
+  document.getElementById('site-nav-close-area').style.width = '0';
+  document.getElementById('site-nav-close-area')
+    .removeEventListener(
+      'transitionend', close_site_nav_transitionend_callback);
+}
+
+function close_site_nav() {
+  document.querySelector('html').style.overflow = 'unset';
+  document.getElementById('site-nav-links').style.width = '0';
+
+  document.getElementById('site-nav-close-area').style.backgroundColor =
+    '#2220';
+  document.getElementById('site-nav-close-area')
+    .addEventListener('transitionend', close_site_nav_transitionend_callback);
 }
